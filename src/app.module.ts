108b205adbebd0be156e464a8ce84acbf573c776
Rafael Queiroz <rafaelproinfo@gmail.com>
2026-03-05 22:59:20 -0300

    🚀 Deploy:  Deploy no Render

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,17 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { DevService } from './data/services/dev.service';
-import { SeguroModule } from './seguro/seguro.module';
-import { ClienteModule } from './cliente/cliente.module';
-import { UsuarioModule } from './usuario/usuario.module';
 import { AuthModule } from './auth/auth.module';
+import { ClienteModule } from './cliente/cliente.module';
+import { ProdService } from './data/services/prod.service';
+import { SeguroModule } from './seguro/seguro.module';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      useClass: DevService,
+      useClass: ProdService,
       imports: [ConfigModule],
     }),
     SeguroModule,
